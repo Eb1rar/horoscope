4aee1a724cd5e94f08d99ad2b9a02bc6b1d5388a
ebrar <ebrkdmr1@gmail.com>
2023-12-05 18:47:20 +0300

    horoscope-project

---
+++ src/components/Header.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "../App.css";
+
+const Header = () => {
+  return (
+    <div className="header">
+      <h2 className="title">Who Are You?</h2>
+    </div>
+  );
+};
+
+export default Header;
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,20 @@
+import "../App.css";
+import logo from "../helper/logo.png";
+
+const Navbar = () => {
+  return (
+    <div className="navbar">
+      <div className="logo">
+        <img src="{logo}" alt="" width="100px" />
+      </div>
+      <div className="links">
+        <a href="#">Horoscope</a>
+        <a href="#">Daily</a>
+        <a href="#">Tarot</a>
+        <a href="#">Article</a>
+        <a href="#">Contact</a>
+      </div>
+    </div>
+  );
+};
+export default Navbar;
